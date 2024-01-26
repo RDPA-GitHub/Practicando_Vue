47b0b6996e64f06d5062a4f5eb8aca28651bdcc0
RDPA-GitHub <ronald.micros@gmail.com>
2024-01-25 23:30:39 -0300

    Creacion code.js, Fecha: 25/01/2024

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -57,84 +57,10 @@
       </div>
     </div>
 
-    <!-- <pre class="text-light fw-bold">
-    {{ $data }}
-    </pre> -->
-
-
-
   </div>
 </template>
 
-<script>
-
-
-export default {
-  name: 'HelloWorld',
-  data() {
-    return {
-      newTasks: "",
-      texto: "Agregando Tareas con VUE.js",
-      tasks: [
-
-        {
-          title: "Aprender PHP",
-          completed: true
-        },
-        {
-          title: "Aprender Laravel",
-          completed: true
-        },
-        {
-          title: "Aprender Vue js",
-          completed: false
-        }
-      ]
-    }
-  },
-  methods: {
-    addTask: function () {
-      this.newTasks.length > 0
-        ? (
-          this.tasks.push({
-            title: this.newTasks,
-            completed: false
-          }),
-          this.newTasks = "")
-        :
-        alert('La tarea no puede estar vacia');
-
-      console.log(this.tasks);
-    }
-  },
-  computed: {
-
-    completedTasks: function() {
-
-      console.log('Completado');
-
-      return this.tasks.filter((task) => {
-        return task.completed;
-      }).length;
-    },
-    IncompletedTasks: function(){
-
-      console.log('No Completado');
-
-      return this.tasks.filter((task) => {
-        return !task.completed;
-      }).length;
-    },
-
-    Totales: function () {
-      return this.tasks.length;
-    }
-  }
-
-}
-</script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+<script src="./js/code,js"></script>
 <style scoped>
 h3 {
   text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
